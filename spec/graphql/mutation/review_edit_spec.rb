6dd33e947e5c8a2e4e1495e5590a33948dbb6dac
Alex Ferencz <alex.ferencz6@gmail.com>
2021-08-30 16:41:43 -0600

    Review edit with tests

--- spec/graphql/mutation/review_edit_spec.rb
+++ spec/graphql/mutation/review_edit_spec.rb
@@ -13,39 +13,39 @@ RSpec.describe 'edit review', type: :request do
           safety_meter: 8
         )
 
-        post graphql_path, params: { query: mutation(review_id: review.id, size: 7, body: "laddy daddy we Like to party") }
+        post graphql_path, params: { query: mutation(review_id: review.id, size: 7.0, body: "laddy daddy we Like to party") }
         json_response = JSON.parse(@response.body, symbolize_names: true)
 
-        expected = [review.id, 7, "laddy daddy we Like to party"]
-
-        actual = []
+        expected = [review.id, 7.0, "laddy daddy we Like to party", review.rating, review.safety_meter]
+        actual = [json_response[:data][:reviewEdit][:review][:id].to_i,
+                  json_response[:data][:reviewEdit][:review][:size].to_f,
+                  json_response[:data][:reviewEdit][:review][:body],
+                  json_response[:data][:reviewEdit][:review][:rating].to_i,
+                  json_response[:data][:reviewEdit][:review][:safetyMeter].to_i]
 
         expect(actual).to eq(expected)
       end
     end
 
-    def mutation(user_id:)
+    def mutation(review_id:, size:, body:)
         <<~GQL
           mutation {
-            reviewCreate(
-              userId: #{user_id},
-              clientEmail: "clientemail@test.com",
-              rating: 3,
-              safetyMeter: 9
+            reviewEdit(
+              reviewId: #{review_id},
+              size: #{size},
+              body: "#{body}"
             )
             {
               review{
                 id
-              }
-              user{
-                id
-              }
-              client{
-                id
+                size
+                body
+                safetyMeter
+                rating
               }
             }
           }
         GQL
     end
-  end
+
 end
