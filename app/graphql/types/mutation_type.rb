f60f3244081eac2b91ee6ba4ce5416fce0829183
Alex Ferencz <alex.ferencz6@gmail.com>
2021-08-30 15:23:20 -0600

    User create with test

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :review_create, mutation: Mutations::ReviewCreate
+    field :user_create, mutation: Mutations::UserCreate
   end
 end
