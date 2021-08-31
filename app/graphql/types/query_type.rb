b733faa47f29bd853f36488457739a1bdc124732
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-08-30 23:01:34 -0600

    Add ability to order ratings by any numerical field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,49 +7,50 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # USER
-    field :all_users, [Types::UserType], null: false
-    def all_users
-      User.all
-  end
+    # USER -----------------------------------------------
 
-    field :single_user, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
-    def single_user(id:)
-      User.find(id)
-    end
+      field :all_users, [Types::UserType], null: false
+      def all_users
+        User.all
+      end
 
-    # CLIENT
-    field :all_clients, [Types::UserType], null: false
-    def all_clients
-      Client.all
-    end
+      field :single_user, Types::UserType, null: false do
+        argument :id, ID, required: true
+      end
+      def single_user(id:)
+        User.find(id)
+      end
 
-    field :single_client, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
-    def single_client(id:)
-      Client.find(id)
-    end
+    # CLIENT -----------------------------------------------
 
-  # REVIEWS
-  field :all_reviews, [Types::ReviewType], null: false
-    def all_reviews
-      Review.all
-    end
+      field :all_clients, [Types::ClientType], null: false
+      def all_clients
+        Client.all
+      end
 
-  field :reviews_by_rating_high_to_low, [Types::ReviewType], null: false
-    def reviews_by_rating_high_to_low
-      Review.order(rating: :desc)
-    end
+      field :single_client, Types::ClientType, null: false do
+        argument :id, ID, required: true
+      end
+      def single_client(id:)
+        Client.find(id)
+      end
 
-  field :single_review, Types::ReviewType, null: false do
-      argument :id, ID, required: true
-    end
-    
-    def single_review(id:)
-      Review.find(id)
-    end
+    # REVIEWS -----------------------------------------------
+
+      field :all_reviews, [Types::ReviewType], null: false do
+        argument :order_by, String, required: false
+      end
+      def all_reviews(order_by:)
+        order_by = 'id' if order_by.empty?
+        Review.order("#{order_by} DESC, id")
+      end 
+
+      field :single_review, Types::ReviewType, null: false do
+        argument :id, ID, required: true
+      end
+      def single_review(id:)
+        Review.find(id)
+      end
+
   end
 end
