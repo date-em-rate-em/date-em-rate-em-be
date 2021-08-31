2540719517dddc99bf19a740a70ce3113dac9239
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-08-30 21:23:30 -0600

    Remove ability to request hashed password from queries

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,7 +2,6 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: false
-    field :password_digest, String, null: false
 
     field :reviews, [Types::ReviewType], null: true
   end
