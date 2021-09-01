169296712235b7e581679addbc1fdd5526493f66
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-09-01 16:57:31 -0600

    add client average safety meter

--- app/graphql/types/client_type.rb
+++ app/graphql/types/client_type.rb
@@ -5,9 +5,14 @@ module Types
 
     field :reviews, [Types::ReviewType], null: true
     field :average_rating, Float, null: true
+    field :average_safety_meter, Float, null: true
 
     def average_rating
       self.object.reviews.average(:rating)
+    end
+
+    def average_safety_meter
+      self.object.reviews.average(:safety_meter)
     end
   end
 end
