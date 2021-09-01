f19d746da1628aed9145d270077670a46ad789fd
Alex Ferencz <alex.ferencz6@gmail.com>
2021-08-31 20:42:23 -0600

    Change client type and spec

--- app/graphql/types/client_type.rb
+++ app/graphql/types/client_type.rb
@@ -4,5 +4,10 @@ module Types
     field :email, String, null: false
 
     field :reviews, [Types::ReviewType], null: true
+    field :average_rating, Integer, null: true
+
+    def average_rating
+      self.object.reviews.average(:rating)
+    end
   end
 end
--- spec/graphql/query/client_spec.rb
+++ spec/graphql/query/client_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'single_client', type: :request  do
   it 'returns a single client' do
     client_1 = Client.create!(email: Faker::Internet.email)
 
-    query_string = "{ singleClient(id: #{client_1.id}) { email } }"
+    query_string = "{ singleClient(id: #{client_1.id}) { averageRating } }"
     post graphql_path(params: { query: query_string })
     json_response = JSON.parse(@response.body, symbolize_names: true)
 
