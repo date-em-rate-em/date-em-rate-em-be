c54f306230cd8d3137dc10b08ac98d8d1d573206
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-09-02 15:33:54 -0600

    Add range validation to reviews with testing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :client
+
+  validates :user, presence: true
+  validates :client, presence: true
+  validates :rating, presence: true
+  validates :safety_meter, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
+  validates :safety_meter, numericality: { greater_than_or_equal_to: 1 }
+  validates :safety_meter, numericality: { less_than_or_equal_to: 10 }
+  validates :size, numericality: { greater_than_or_equal_to: 1 }
+  validates :size, numericality: { less_than_or_equal_to: 18 }
+  validates :payment, numericality: { greater_than_or_equal_to: 1 }
+  validates :payment, numericality: { less_than_or_equal_to: 10 }
+  validates :kindness, numericality: { greater_than_or_equal_to: 1 }
+  validates :kindness, numericality: { less_than_or_equal_to: 10 }
+  validates :vibe, numericality: { greater_than_or_equal_to: 1 }
+  validates :vibe, numericality: { less_than_or_equal_to: 10 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,8 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-    describe 'relationships' do
+  describe 'relationships' do
     it {should belong_to(:user)}
     it {should belong_to(:client)}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:client) }
+    it { should validate_presence_of(:rating) }
+    it { should validate_presence_of(:safety_meter) }
+
+    it do
+      should validate_numericality_of(:rating).
+      is_greater_than_or_equal_to(1).
+        is_less_than_or_equal_to(5)
+    end
+
+    it do
+      should validate_numericality_of(:safety_meter).
+      is_greater_than_or_equal_to(1).
+        is_less_than_or_equal_to(10)
+    end
+
+    it do
+      should validate_numericality_of(:payment).
+      is_greater_than_or_equal_to(1).
+        is_less_than_or_equal_to(10)
+    end
+
+    it do
+      should validate_numericality_of(:kindness).
+      is_greater_than_or_equal_to(1).
+        is_less_than_or_equal_to(10)
+    end
+
+    it do
+      should validate_numericality_of(:vibe).
+      is_greater_than_or_equal_to(1).
+        is_less_than_or_equal_to(10)
+    end
+
+    it do
+      should validate_numericality_of(:size).
+      is_greater_than_or_equal_to(1).
+        is_less_than_or_equal_to(18)
+    end
+    
   end
 end
