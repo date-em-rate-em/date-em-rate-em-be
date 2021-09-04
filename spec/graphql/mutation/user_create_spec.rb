b3f965e50fad1c7e31b4be1fd29c4b6c72720ae3
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-09-03 23:38:32 -0600

    Add tests for all sad paths

--- spec/graphql/mutation/user_create_spec.rb
+++ spec/graphql/mutation/user_create_spec.rb
@@ -2,21 +2,65 @@ require 'rails_helper'
 
 RSpec.describe 'create user', type: :request do
   describe 'mutation: userCreate' do
-    it 'creates client and review' do
+    describe 'happy path' do
+      it 'creates client and review' do
 
-      post graphql_path, params: { query: mutation(
-        email: "email@test.com",
-        password: "password",
-        password_confirmation: "password"
-        ) }
+        post graphql_path, params: { query: mutation(
+          email: "email@test.com",
+          password: "password",
+          password_confirmation: "password"
+          ) }
 
-      json_response = JSON.parse(response.body, symbolize_names: true)
+        json_response = JSON.parse(response.body, symbolize_names: true)
 
-      actual = json_response[:data][:userCreate][:user][:email]
+        actual = json_response[:data][:userCreate][:user][:email]
 
-      expect(actual).to eq("email@test.com")
+        expect(actual).to eq("email@test.com")
+      end
+    end
+    
+  describe 'sad path' do
+    describe 'passwords do not match' 
+      it 'returns an error' do
+        post graphql_path, params: { query: mutation(
+          email: "email@test.com",
+          password: "password",
+          password_confirmation: "password_1"
+          ) }
+
+        json_response = JSON.parse(response.body, symbolize_names: true)
+
+        actual = [json_response[:errors][0][:message], response.status]
+        expected = ["Validation failed: Password confirmation doesn't match Password", 405]
+
+        expect(actual).to eq(expected)
+      end
+    end
+
+    describe 'email already used' do
+      it 'returns an error' do
+        post graphql_path, params: { query: mutation(
+          email: "email@test.com",
+          password: "password",
+          password_confirmation: "password"
+          ) }
+
+          post graphql_path, params: { query: mutation(
+            email: "email@test.com",
+            password: "password",
+            password_confirmation: "password"
+            ) }
+
+        json_response = JSON.parse(response.body, symbolize_names: true)
+
+        actual = [json_response[:errors][0][:message], response.status]
+        expected = ["Validation failed: Email has already been taken", 405]
+
+        expect(actual).to eq(expected)
+      end
     end
   end
+
 
   def mutation(email:, password:, password_confirmation:)
     <<~GQL
