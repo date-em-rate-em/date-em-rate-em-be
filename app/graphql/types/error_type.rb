1b7655bf19b51ffe4e37f5a4e51a9bdee8f72fb8
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-09-03 18:38:23 -0600

    Switch to rescues per front end request

--- app/graphql/types/error_type.rb
+++
@@ -1,6 +0,0 @@
-module Types
-  class ErrorType < Types::BaseObject
-    field :message, String, null: false
-    field :status, Integer, null: false
-  end
-end
--- app/poros/error.rb
+++
@@ -1,9 +0,0 @@
-class Error
-  attr_reader :message, :status
-
-  def initialize(attributes)
-    @message = attributes[:message]
-    @status = attributes[:status]
-  end
-
-end
