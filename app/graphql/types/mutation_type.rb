d2e2bf26fc5bdc9a8c4e589fcb12e7faf92a8155
Alex Ferencz <alex.ferencz6@gmail.com>
2021-10-07 15:26:47 -0600

    Add sign in to mutation type
    Co-authored-by: Lawrence <ldwhalen@gmail.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :review_create, mutation: Mutations::ReviewCreate
     field :user_create, mutation: Mutations::UserCreate
     field :review_edit, mutation: Mutations::ReviewEdit
+    field :sign_in, mutation: Mutations::SignIn
   end
 end
