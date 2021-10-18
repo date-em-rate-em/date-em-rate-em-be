6067c5fe3dd5670bab2011c8061ff81a5937fa9b
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-10-18 16:10:41 -0600

    Modify review create test for addition of phone number to client
    Co-authored-by: Alex <alex.ferencz6@gmail.com>

--- app/validations/client_identifiers_validation.rb
+++
@@ -1,7 +0,0 @@
-class ClientIdentfiersValidation < ActiveModel::Validator
-  def validate(record)
-    unless(record.email || record.phone)
-      record.errors.add :email, "A review requires either an email or phone number to identify the client."
-    end
-  end
-end
--- spec/graphql/mutation/review_create_spec.rb
+++ spec/graphql/mutation/review_create_spec.rb
@@ -3,50 +3,124 @@ require 'rails_helper'
 
 RSpec.describe 'create review', type: :request do
   describe 'mutation: reviewCreate' do
-    describe 'client does not exist' do
-      it 'creates client and review' do
-        @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
+    describe 'create client by email' do
+      describe 'client does not exist' do
+        it 'creates client and review' do
+          @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
 
 
-        post graphql_path, params: { query: mutation(user_id: @user.id, rating: 3) }
-        json_response = JSON.parse(@response.body, symbolize_names: true)
+          post graphql_path, params: { 
+            query: mutation(
+              user_id: @user.id, 
+              rating: 3, 
+              email: 'clientemail@test.com',
+              phone: nil) 
+            }
+          json_response = JSON.parse(@response.body, symbolize_names: true)
 
-        expected = [@user.id, Review.first.client_id, Review.first.id]
+          expected = [@user.id, Review.first.client_id, Review.first.id]
 
-        actual = [
-          json_response[:data][:reviewCreate][:user][:id].to_i,
-          json_response[:data][:reviewCreate][:client][:id].to_i,
-          json_response[:data][:reviewCreate][:review][:id].to_i
-        ]
+          actual = [
+            json_response[:data][:reviewCreate][:user][:id].to_i,
+            json_response[:data][:reviewCreate][:client][:id].to_i,
+            json_response[:data][:reviewCreate][:review][:id].to_i
+          ]
 
-        expect(actual).to eq(expected)
+          expect(actual).to eq(expected)
+        end
+      end
+      describe 'client does exist' do
+        it 'creates review and assigns to client' do
+          @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
+          @client = Client.create!(email: 'clientemail@test.com')
+
+          post graphql_path, params: { 
+            query: mutation(
+              user_id: @user.id, 
+              rating: 3, 
+              email: 'clientemail@test.com',
+              phone: nil) 
+            }
+          json_response = JSON.parse(@response.body, symbolize_names: true)
+
+          expected = [@user.id, @client.id, Review.first.id]
+
+          actual = [
+            json_response[:data][:reviewCreate][:user][:id].to_i,
+            json_response[:data][:reviewCreate][:client][:id].to_i,
+            json_response[:data][:reviewCreate][:review][:id].to_i
+          ]
+
+          expect(actual).to eq(expected)
+        end
       end
     end
-    describe 'client does exist' do
-      it 'creates review and assigns to client' do
-        @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
-        @client = Client.create!(email: 'clientemail@test.com')
 
-        post graphql_path, params: { query: mutation(user_id: @user.id, rating: 3) }
-        json_response = JSON.parse(@response.body, symbolize_names: true)
+    describe 'create client by phone number' do
+      describe 'client does not exist' do
+        it 'creates client and review' do
+          @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
 
-        expected = [@user.id, @client.id, Review.first.id]
 
-        actual = [
-          json_response[:data][:reviewCreate][:user][:id].to_i,
-          json_response[:data][:reviewCreate][:client][:id].to_i,
-          json_response[:data][:reviewCreate][:review][:id].to_i
-        ]
+          post graphql_path, params: { 
+            query: mutation(
+              user_id: @user.id, 
+              rating: 3, 
+              email: nil,
+              phone: 5555555555) 
+            }
+          json_response = JSON.parse(@response.body, symbolize_names: true)
 
-        expect(actual).to eq(expected)
+          expected = [@user.id, Review.first.client_id, Review.first.id]
+
+          actual = [
+            json_response[:data][:reviewCreate][:user][:id].to_i,
+            json_response[:data][:reviewCreate][:client][:id].to_i,
+            json_response[:data][:reviewCreate][:review][:id].to_i
+          ]
+
+          expect(actual).to eq(expected)
+        end
+      end
+      describe 'client does exist' do
+        it 'creates review and assigns to client' do
+          @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
+          @client = Client.create!(phone: 5555555555)
+
+          post graphql_path, params: { 
+            query: mutation(
+              user_id: @user.id, 
+              rating: 3, 
+              email: nil,
+              phone: 5555555555) 
+            }
+          json_response = JSON.parse(@response.body, symbolize_names: true)
+
+          expected = [@user.id, @client.id, Review.first.id]
+
+          actual = [
+            json_response[:data][:reviewCreate][:user][:id].to_i,
+            json_response[:data][:reviewCreate][:client][:id].to_i,
+            json_response[:data][:reviewCreate][:review][:id].to_i
+          ]
+
+          expect(actual).to eq(expected)
+        end
       end
     end
+
     describe 'review does not save' do
       it 'does not save' do
         @user = User.create!(email: 'testemail@test.com', password: 'testpassword', password_confirmation: 'testpassword')
         @client = Client.create!(email: 'clientemail@test.com')
 
-        post graphql_path, params: { query: mutation(user_id: @user.id, rating: 8) }
+        post graphql_path, params: { 
+          query: mutation(
+            user_id: @user.id, 
+            rating: 8, 
+            email: 'clientemail@test.com',
+            phone: nil) 
+          }
         json_response = JSON.parse(@response.body, symbolize_names: true)
 
         actual = [json_response[:errors][0][:message], response.status]
@@ -56,12 +130,13 @@ RSpec.describe 'create review', type: :request do
       end
     end
 
-    def mutation(user_id:, rating:)
+    def mutation(user_id:, rating:, email:, phone:)
         <<~GQL
           mutation {
             reviewCreate(
               userId: #{user_id},
-              clientEmail: "clientemail@test.com",
+              clientPhone: "#{phone}",
+              clientEmail: "#{email}",
               rating: #{rating},
               safetyMeter: 9
             )
