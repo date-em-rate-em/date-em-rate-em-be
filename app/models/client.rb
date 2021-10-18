28c4c195eec6a00618acd1795b972fc28fe720bf
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-10-18 16:02:48 -0600

    Add and test custom validation for client
    Co-authored-by: Alex <alex.ferencz6@gmail.com>

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,12 @@
 class Client < ApplicationRecord
-  include ActiveModel::Validations
-  validates_with ClientIdentfiersValidation
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
+  validate :client_must_have_identifier
+
+  def client_must_have_identifier
+    unless email.present? || phone.present?
+      errors.add(:email, "Reviews must have a client email or phone number.")
+      errors.add(:phone, "Reviews must have a client email or phone number.")
+    end
+  end
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Client, type: :model do
     it {should have_many(:reviews)}
     it {should have_many(:users).through(:reviews)}
   end
+  describe 'validations' do
+    describe 'custom validation' do
+      it 'should have an email and/or phone number' do
+        client = Client.new
+        client.valid?
+
+        expect(client.errors.messages.count).to eq(2)
+      end
+    end
+  end
 end
